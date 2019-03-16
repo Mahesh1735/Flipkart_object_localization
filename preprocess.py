98c60225977437dbd52c25d708f5522005222a6a
Mukul Ranjan <mukulranjaniitg@gmail.com>
2019-03-16 07:13:07 +0530

    Update and rename utilmodopti.py to preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -10,6 +10,7 @@ for index, row in df.iterrows():
     path = './images/' + row['image_name']
     img = cv2.imread(path)
     #edges = np.multiply(0.25,cv2.Canny(img,50,99)) + np.multiply(0.5,cv2.Canny(img,100,149)) + np.multiply(0.75,cv2.Canny(img,150,199)) + cv2.Canny(img,200,255)
+    #added extra channel edges as our 4th channel which incresed our iou in 2nd level from 0.62 to 0.696396
     edges = cv2.Canny(img,50,255)
     #print(edges[:,:,np.newaxis].shape)
     img = np.concatenate((img,edges[:,:,np.newaxis]), axis = 2)
